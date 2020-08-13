e647f5c3a041b4a32dfcb53a646416f0fb1820de
Levon Aarakelyan <fizmatstepanakert@gmail.com>
2020-08-13 21:02:58 +0400

    Added car on endless road

---
+++ Racer/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+  GameObject[] roads;
+  Camera mainCamera;
+  Vector2 cameraMetrics;
+  Vector2 roadMetrics;
+
+  const float speed = 0.05f;
+  const string roadTag = "Road";
+
+  void Start() {
+    mainCamera = GetComponent<Camera>();
+    roads = GameObject.FindGameObjectsWithTag(roadTag);
+
+    cameraMetrics = new Vector2(2f * mainCamera.orthographicSize * mainCamera.aspect, 2f * mainCamera.orthographicSize);
+    roadMetrics = roads[0].GetComponent<Renderer>().bounds.size;
+  }
+
+  void Update() {
+    ChangeRoadsPosition();
+  }
+
+  void ChangeRoadsPosition() {
+    Vector2 cameraPos = transform.position;
+    for(int i = 0; i < roads.Length; i++) {
+      Vector2 roadPos = roads[i].transform.position;
+      if(roadPos.y + roadMetrics.y / 2 <= cameraPos.y - cameraMetrics.y / 2) {
+        roads[i].transform.position = new Vector2(roadPos.x, cameraPos.y + cameraMetrics.y / 2 + roadMetrics.y / 2);
+      } else {
+        roads[i].transform.position = new Vector2(roadPos.x, roadPos.y - speed);
+      }
+    }
+  }
+}
---
+++ Racer/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+  public Camera cam;
+  Vector2 cameraMetrics;
+  Vector2 carMetrics;
+
+  void Start() {
+    cameraMetrics = new Vector2(2f * cam.orthographicSize * cam.aspect, 2f * cam.orthographicSize);
+    carMetrics = GetComponent<Renderer>().bounds.size;
+  }
+
+  void Update() {
+    move();
+  }
+
+  void move(){
+    Vector3 carPos = transform.position;
+    Vector3 camPos = cam.transform.position;
+    float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+
+    // Borders: left -> right -> top -> down
+    if(carPos.x - carMetrics.x / 2 - 1 < camPos.x - cameraMetrics.x / 2) {
+      if(horizontal < 0) {
+        horizontal = 0f;
+      }
+    }
+
+    if(carPos.x + carMetrics.x / 2 + 0.9 > camPos.x + cameraMetrics.x / 2) {
+      if(horizontal > 0) {
+        horizontal = 0f;
+      }
+    }
+    
+    if(carPos.y + carMetrics.y / 2 > camPos.y + cameraMetrics.y / 2) {
+      if(vertical > 0) {
+        vertical = 0f;
+      }
+    }
+
+    if(carPos.y - carMetrics.y / 2 < camPos.y - cameraMetrics.y / 2) {
+      if(vertical < 0) {
+        vertical = 0f;
+      }
+    } 
+	
+    transform.position += new Vector3(horizontal, vertical, 0f) * Time.deltaTime * 7f;
+  }
+}
